02217aa373dd03e2e4f48e71c5388a235fdd6aed
Gary50613 <50613gary@gmail.com>
2023-07-20 15:11:41 +0800

    fix headers

--- src/pages/api/stats.tsx
+++ src/pages/api/stats.tsx
@@ -1,19 +1,21 @@
 import { createClient } from "@vercel/edge-config";
+import { readFile } from "fs/promises";
 import satori from "satori";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const formatter = new Intl.NumberFormat();
 
-const interPromise = fetch(
+const interPromise = readFile(
 	new URL("../../fonts/Inter-Bold.woff", import.meta.url),
-).then((r) => r.arrayBuffer());
+);
 
-const notoPromise = fetch(
+const notoPromise = readFile(
 	new URL("../../fonts/NotoSansTC-Medium.woff", import.meta.url),
-).then((r) => r.arrayBuffer());
+);
 
-const notoBoldPromise = fetch(
+const notoBoldPromise = readFile(
 	new URL("../../fonts/NotoSansTC-Bold.woff", import.meta.url),
-).then((r) => r.arrayBuffer());
+);
 
 const edgeConfig =
 	process.env.EDGE_CONFIG ||
@@ -22,103 +24,105 @@ const edgeConfig =
 
 const client = edgeConfig ? createClient(edgeConfig) : undefined;
 
-export default async function Stats() {
-	const [inter, noto, notoBold] = await Promise.all([interPromise, notoPromise, notoBoldPromise]) ;
+export default async function Stats(req: NextApiRequest, res: NextApiResponse) {
+	const [inter, noto, notoBold] = await Promise.all([
+		interPromise,
+		notoPromise,
+		notoBoldPromise,
+	]);
 
-	const guilds = (await client?.get("guilds") as number) || 123456;
+	const guilds = ((await client?.get("guilds")) as number) || 123456;
 
 	const image = await satori(
-		(
+		<div
+			lang="zh-TW"
+			style={{
+				background: "linear-gradient(135deg, #3a3f5c, #202332)",
+				borderRadius: 7,
+				color: "white",
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				textAlign: "center",
+				padding: 16,
+				flexDirection: "column",
+				gap: 16,
+			}}
+		>
 			<div
-				lang="zh-TW"
 				style={{
-					background: "linear-gradient(135deg, #3a3f5c, #202332)",
-					borderRadius: 7,
-					color: "white",
-					width: "100%",
-					height: "100%",
 					display: "flex",
-					textAlign: "center",
-					padding: 16,
-					flexDirection: "column",
 					gap: 16,
 				}}
 			>
-				<div
+				<img
+					alt="Logo"
+					src={
+						process.env.BOT_AVATAR_URL ||
+						"https://yeecord.com/img/logo.png"
+					}
+					width={56}
+					height={56}
 					style={{
-						display: "flex",
-						gap: 16,
+						borderRadius: 50,
 					}}
-				>
-					<img
-						alt="Logo"
-						src={
-							process.env.BOT_AVATAR_URL ||
-							"https://yeecord.com/img/logo.png"
-						}
-						width={56}
-						height={56}
-						style={{
-							borderRadius: 50,
-						}}
-					/>
-					<div
-						style={{
-							display: "flex",
-							flexDirection: "column",
-							alignContent: "space-between",
-						}}
-					>
-						<span
-							style={{
-								fontSize: 24,
-								fontWeight: 700,
-							}}
-						>
-							YEE式機器龍
-						</span>
-						<span
-							style={{
-								color: "#c4c4c4",
-							}}
-						>
-							萬中選一的 Discord 機器人
-						</span>
-					</div>
-				</div>
-				<span>正在服務</span>
+				/>
 				<div
 					style={{
 						display: "flex",
-						alignItems: "flex-end",
-						gap: 16,
-						marginTop: 8,
+						flexDirection: "column",
+						alignContent: "space-between",
 					}}
 				>
 					<span
 						style={{
-							fontFamily: "Inter",
-							fontSize: 36,
+							fontSize: 24,
 							fontWeight: 700,
-							backgroundImage:
-								"linear-gradient(45deg, #f35e5e, #ae4edf)",
-							backgroundClip: "text",
-							color: "transparent",
 						}}
 					>
-						{formatter.format(guilds)}
+						YEE式機器龍
 					</span>
 					<span
 						style={{
-							paddingBottom: 4,
-							fontSize: 20,
+							color: "#c4c4c4",
 						}}
 					>
-						個伺服器
+						萬中選一的 Discord 機器人
 					</span>
 				</div>
 			</div>
-		),
+			<span>正在服務</span>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "flex-end",
+					gap: 16,
+					marginTop: 8,
+				}}
+			>
+				<span
+					style={{
+						fontFamily: "Inter",
+						fontSize: 36,
+						fontWeight: 700,
+						backgroundImage:
+							"linear-gradient(45deg, #f35e5e, #ae4edf)",
+						backgroundClip: "text",
+						color: "transparent",
+					}}
+				>
+					{formatter.format(guilds)}
+				</span>
+				<span
+					style={{
+						paddingBottom: 4,
+						fontSize: 20,
+					}}
+				>
+					個伺服器
+				</span>
+			</div>
+		</div>,
 		{
 			width: 480,
 			height: 180,
@@ -145,10 +149,10 @@ export default async function Stats() {
 		},
 	);
 
-	return new Response(image, {
-		headers: {
-			"content-type": "image/svg+xml",
-			"cache-control": "public, immutable, no-transform, max-age=60",
-		},
-	})
+	res.appendHeader("Content-Type", "image/svg+xml")
+		.appendHeader(
+			"Cache-Control",
+			"public, immutable, no-transform, max-age=60",
+		)
+		.send(image);
 }
